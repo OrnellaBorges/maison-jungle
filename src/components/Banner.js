4d090193bc12aa15aaac1039fc35b5099f5a2925
Ornella B <borges.ornella@gmail.com>
2023-04-18 15:48:20 +0200

    cleaning + css lesson : Incorporez du style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,16 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
 function Banner() {
 	const title = 'La maison jungle'
 	return (
-        <div className="lmj-banner">
+        <header className="lmj-banner">
+            <img src={logo}
+                 alt='la maison jungle'
+                 className='lmj-logo'/>
             <h1>{title}</h1>
-        </div>
+        </header>
+        
     )
 }
 
